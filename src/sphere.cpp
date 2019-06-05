3459b8aa4bed4f965a448816eadcca871ec07b54
Vishal Sontakke <sontakke.vishal1991@gmail.com>
2019-06-05 10:07:40 +0530

    Fix: origin was not being set to 0,0,0

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -4,7 +4,7 @@ Sphere::Sphere() : uSubDiv(10), vSubDiv(10), radius(1.0)
 {
     this->origin[0] = 0.0;
     this->origin[1] = 0.0;
-    this->origin[1] = 0.0;
+    this->origin[2] = 0.0;
 }
 
 Sphere::~Sphere()
