65a462e5421cd9b06bd67f8fe9cddf0c9c7e955f
Vishal Sontakke <sontakke.vishal1991@gmail.com>
2019-10-18 20:40:42 +0530

    Add constructor which takes a file name for PPMWriter

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "sphere.hpp"
+#include "ppmwriter.hpp"
 
 int main(int argc, char const *argv[])
 {
+    PPMWriter outputImage();
     Sphere s(RGBColor{45});
 
     std::cout << "Test\n";
