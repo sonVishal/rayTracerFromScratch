55b32692e12bb71743ac95410394bd5e3eadd53b
Vishal Sontakke <sontakke.vishal1991@gmail.com>
2019-10-19 10:40:01 +0530

    Add quadratic eq solver

---
+++ include/quadratic.hpp
@@ -0,0 +1,24 @@
+#include <array>
+#include <memory>
+#include <cmath>
+
+class QuadraticEq
+{
+private:
+    std::array<double, 3> m_coeffs{0.0};
+    std::array<double, 2> m_roots{std::nan("0.0")};
+    int m_numRoots{0};
+    double CalculateDeterminant() const;
+public:
+    QuadraticEq();
+    // ax² + bx + c = 0 form
+    void SetEquation(double t_a, double t_b, double t_c);
+    // Calculates the roots and returns number of roots.
+    int CalculateRoots();
+    // Gets the number of roots and does not calculate
+    int GetNumberOfRoots() const;
+    const std::array<double, 2>& GetRoots() const;
+    double GetDeterminant() const;
+    void PrintEquation() const;
+    ~QuadraticEq();
+};
---
+++ src/quadratic.cpp
@@ -0,0 +1,88 @@
+#include "quadratic.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <cmath>
+
+QuadraticEq::QuadraticEq(/* args */)
+{
+}
+
+QuadraticEq::~QuadraticEq()
+{
+}
+
+// ax² + bx + c = 0 form
+void QuadraticEq::SetEquation(double t_a, double t_b, double t_c)
+{
+    m_coeffs.at(0) = t_a;
+    m_coeffs.at(1) = t_b;
+    m_coeffs.at(2) = t_c;
+}
+
+double QuadraticEq::CalculateDeterminant() const
+{
+    double bSq = m_coeffs.at(1) * m_coeffs.at(1);
+    double ac4 = 4 * m_coeffs.at(0) * m_coeffs.at(2);
+    return bSq - ac4;
+}
+
+int QuadraticEq::GetNumberOfRoots() const
+{
+    const double determinant = CalculateDeterminant();
+    int numRoots = 0;
+    if (determinant == 0.0)
+    {
+        numRoots = 1;
+    }
+    else if (determinant > 0.0)
+    {
+        numRoots = 2;
+    }
+    return numRoots;
+}
+
+int QuadraticEq::CalculateRoots()
+{
+    const double determinant = CalculateDeterminant();
+    int numRoots = 0;
+    if (determinant == 0.0)
+    {
+        m_roots.at(0) = -m_coeffs.at(1) * 0.5 / m_coeffs.at(0);
+        m_roots.at(1) = m_roots.at(0);
+        numRoots = 1;
+    }
+    else if (determinant > 0.0)
+    {
+        double tmpRes = std::fabs(m_coeffs.at(1)) + sqrt(determinant);
+        tmpRes *= std::signbit(m_coeffs.at(1)) ? 1.0 : -1.0;
+        m_roots.at(0) = tmpRes * 0.5 / m_coeffs.at(0);
+        m_roots.at(1) = 2.0 * m_coeffs.at(2) / tmpRes;
+        if (m_roots.at(0) > m_roots.at(1))
+        {
+            std::swap(m_roots.at(0), m_roots.at(1));
+        }
+        numRoots = 2;
+    }
+    return numRoots;
+}
+
+const std::array<double, 2>& QuadraticEq::GetRoots() const
+{
+    return m_roots;
+}
+
+double QuadraticEq::GetDeterminant() const
+{
+    return CalculateDeterminant();
+}
+
+void QuadraticEq::PrintEquation() const
+{
+    std::streamsize ss = std::cout.precision();
+    std::cout << std::setprecision(4)\
+    << "Solving: " << m_coeffs.at(0) << "x^2 "\
+    << std::showpos << m_coeffs.at(1) << "x "\
+    << m_coeffs.at(2) << " = 0\n";
+    std::cout << std::noshowpos << std::setprecision(ss);
+}
