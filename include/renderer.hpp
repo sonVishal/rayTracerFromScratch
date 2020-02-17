9f1eb1cd5ae27e3915307d7f323d6bf51113e692
Vishal Sontakke <sontakke.vishal1991@gmail.com>
2020-02-17 22:06:19 +0530

    Refactor renderer

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -14,24 +14,46 @@ private:
     double m_aspectRatio;
     double m_maxRes{100};
     unsigned int m_maxBounces{5};
+    unsigned int m_raysPerPixel{1};
+
+    void TraceRay(const Vector3 &t_rayDir, const Vector3 &t_rayOrigin,
+                  const std::vector<Object *> t_objList, png::rgba_pixel &t_pixelColor) const;
+
+    void GetImagePlaneInfo(Vector3 &t_imagePlaneTopLeft,
+                           Vector3 &t_imagePlaneRightDir,
+                           Vector3 &t_imagePlaneDownDir,
+                           unsigned int &t_xPixels,
+                           unsigned int &t_yPixels,
+                           double &t_pixelWidth,
+                           double &t_pixelHeight) const;
+
+    void GetRayInfo(Vector3 &t_rayOrigin, Vector3 &t_rayDirection,
+                    const Vector3 &t_imagePlaneTopLeft,
+                    const Vector3 &t_imagePlaneRightDir,
+                    const Vector3 &t_imagePlaneDownDir,
+                    double t_pixelSize, double t_rightOffset,
+                    double t_downOffset,
+                    unsigned int t_xIdx, unsigned int t_yIdx) const;
 
 public:
     Renderer() = default;
     ~Renderer() = default;
     void SetScene(const Scene &t_scene);
     void SetCamera(const Camera &t_camera);
-    void WriteRenderedImage(const char* t_fileName);
+    void WriteRenderedImage(const char *t_fileName);
     void Render();
 
     void SetAspectRatio(double t_aspectRatio);
     void SetResolution(double t_res);
     void SetMaxBounces(unsigned int t_maxBounces);
+    void SetSamplesPerPixel(unsigned int t_numSamples);
 
     double GetAspectRatio() const;
     double GetResolution() const;
     unsigned int GetMaxBounces() const;
+    unsigned int GetSamplesPerPixel() const;
 
-    void GetImageResolution(std::array<unsigned int, 2> &t_imageRes);
+    void GetImageResolution(std::array<unsigned int, 2> &t_imageRes) const;
 };
 
 #endif //_RENDERER_HPP_
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -31,16 +31,52 @@ double Renderer::GetResolution() const
     return m_maxRes;
 }
 
-void Renderer::GetImageResolution(std::array<unsigned int, 2> &t_imageRes)
+void Renderer::GetImageResolution(std::array<unsigned int, 2> &t_imageRes) const
 {
     t_imageRes.at(1) = m_maxRes;
     t_imageRes.at(0) = m_aspectRatio * t_imageRes.at(1);
 }
 
-void Renderer::Render()
+void Renderer::TraceRay(const Vector3 &t_rayDir, const Vector3 &t_rayOrigin,
+                        const std::vector<Object *> t_objList,
+                        png::rgba_pixel &t_pixelColor) const
+{
+    t_pixelColor = m_sceneToRender.GetAmbientColor();
+    for (size_t i = 0; i < m_maxBounces; i++)
+    {
+        for (auto obj : t_objList)
+        {
+            std::vector<Vector3> intersectionPts;
+            obj->GetIntersectionWithRay(t_rayDir, t_rayOrigin, intersectionPts);
+
+            if (!intersectionPts.empty())
+            {
+                t_pixelColor = obj->GetColor();
+            }
+        }
+    }
+}
+
+unsigned int Renderer::GetSamplesPerPixel() const
+{
+    return m_raysPerPixel;
+}
+
+void Renderer::SetSamplesPerPixel(unsigned int t_numSamples)
+{
+    m_raysPerPixel = t_numSamples;
+}
+
+void Renderer::GetImagePlaneInfo(Vector3 &t_imagePlaneTopLeft,
+                                 Vector3 &t_imagePlaneRightDir,
+                                 Vector3 &t_imagePlaneDownDir,
+                                 unsigned int &t_xPixels,
+                                 unsigned int &t_yPixels,
+                                 double &t_pixelWidth,
+                                 double &t_pixelHeight) const
 {
     std::array<unsigned int, 2> resolution;
-    GetImageResolution(resolution);
+    this->GetImageResolution(resolution);
 
     double revisedAspectRatio = static_cast<double>(resolution.at(0)) / static_cast<double>(resolution.at(1));
 
@@ -48,48 +84,64 @@ void Renderer::Render()
     double imagePlaneWidth = revisedAspectRatio * imagePlaneHeight;
 
     Vector3 imagePlaneCenter = m_camera.GetImagePlaneCenter();
-    Vector3 imagePlaneTopLeft = imagePlaneCenter +
-                                m_camera.GetLeftDirection() * imagePlaneWidth * 0.5 +
-                                m_camera.GetUpDirection() * imagePlaneHeight * 0.5;
+    t_imagePlaneTopLeft = imagePlaneCenter +
+                          m_camera.GetLeftDirection() * imagePlaneWidth * 0.5 +
+                          m_camera.GetUpDirection() * imagePlaneHeight * 0.5;
 
-    Vector3 imagePlaneRightDir = m_camera.GetLeftDirection() * -1.0;
-    Vector3 imagePlaneDownDir = m_camera.GetUpDirection() * -1.0;
+    t_imagePlaneRightDir = m_camera.GetLeftDirection() * -1.0;
+    t_imagePlaneDownDir = m_camera.GetUpDirection() * -1.0;
 
-    double pixelWidth = imagePlaneWidth / resolution.at(0);
-    double pixelHeight = imagePlaneHeight / resolution.at(1);
+    t_xPixels = resolution.at(0);
+    t_yPixels = resolution.at(1);
+
+    t_pixelWidth = imagePlaneWidth / resolution.at(0);
+    t_pixelHeight = imagePlaneHeight / resolution.at(1);
+}
+
+void Renderer::GetRayInfo(Vector3 &t_rayOrigin, Vector3 &t_rayDirection,
+                          const Vector3 &t_imagePlaneTopLeft,
+                          const Vector3 &t_imagePlaneRightDir,
+                          const Vector3 &t_imagePlaneDownDir,
+                          double t_pixelSize, double t_rightOffset,
+                          double t_downOffset,
+                          unsigned int t_xIdx, unsigned int t_yIdx) const
+{
+    Vector3 rightOffsetDir = t_imagePlaneRightDir * t_pixelSize;
+    Vector3 downOffsetDir = t_imagePlaneDownDir * t_pixelSize;
+
+    Vector3 pixelTopLeft = t_imagePlaneTopLeft + rightOffsetDir * t_xIdx + downOffsetDir * t_yIdx;
+    t_rayOrigin = pixelTopLeft + rightOffsetDir * t_rightOffset +
+                  downOffsetDir * t_downOffset;
+    t_rayDirection = t_rayOrigin - m_camera.GetOrigin();
+    t_rayDirection.Normalize();
+}
+
+void Renderer::Render()
+{
+    Vector3 imagePlaneTopLeft, imagePlaneRightDir, imagePlaneDownDir;
+    double pixelWidth = 0.0, pixelHeight = 0.0;
+    unsigned int xPixels = 0, yPixels = 0;
+
+    GetImagePlaneInfo(imagePlaneTopLeft, imagePlaneRightDir, imagePlaneDownDir,
+                      xPixels, yPixels, pixelWidth, pixelHeight);
+
+    m_renderedScene.resize(xPixels, yPixels);
 
     double pixelXoffset = 0.5, pixelYoffset = 0.5;
 
-    m_renderedScene.resize(resolution.at(0), resolution.at(1));
-
-    for (unsigned int y = 0; y < resolution.at(1); y++)
+    for (unsigned int y = 0; y < yPixels; y++)
     {
-        for (unsigned int x = 0; x < resolution.at(0); x++)
+        for (unsigned int x = 0; x < xPixels; x++)
         {
             Vector3 rayOrigin, rayDirection;
-            Vector3 rightOffset = imagePlaneRightDir * pixelWidth;
-            Vector3 downOffset = imagePlaneDownDir * pixelHeight;
 
-            Vector3 pixelTopLeft = imagePlaneTopLeft + rightOffset * x + downOffset * y;
-            rayOrigin = pixelTopLeft + rightOffset * pixelXoffset +
-                        downOffset * pixelYoffset;
-            rayDirection = rayOrigin - m_camera.GetOrigin();
-            rayDirection.Normalize();
+            GetRayInfo(rayOrigin, rayDirection, imagePlaneTopLeft,
+                       imagePlaneRightDir, imagePlaneDownDir, pixelWidth,
+                       pixelXoffset, pixelYoffset, x, y);
 
-            png::rgba_pixel pixelColor(m_sceneToRender.GetAmbientColor());
-
-            auto objList = m_sceneToRender.GetObjectList();
-            for (auto obj : objList)
-            {
-                std::vector<Vector3> intersectionPts;
-                obj->GetIntersectionWithRay(rayDirection, rayOrigin, intersectionPts);
-
-                if (!intersectionPts.empty())
-                {
-                    pixelColor = obj->GetColor();
-                }
-            }
-            m_renderedScene[y][x] = pixelColor;
+            TraceRay(rayDirection, rayOrigin,
+                     m_sceneToRender.GetObjectList(),
+                     m_renderedScene[y][x]);
         }
     }
 }
